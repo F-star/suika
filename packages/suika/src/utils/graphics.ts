be042dabe2dd0d7c6ddcde3df78cc1d47118a450
xigua <hhbuhuiai@163.com>
2023-01-09 11:52:01 +0800

    fix: 修复 isRectContain 方法

--- packages/suika/src/utils/graphics.ts
+++ packages/suika/src/utils/graphics.ts
@@ -89,7 +89,7 @@ export function isPointInCircle(point: IPoint, circle: ICircle) {
 export function isRectContain(rect1: IRect, rect2: IRect) {
   return (
     rect1.x <= rect2.x &&
-    rect2.y <= rect2.y &&
+    rect1.y <= rect2.y &&
     rect1.x + rect1.width >= rect2.x + rect2.width &&
     rect1.y + rect1.height >= rect2.y + rect2.height
   );
