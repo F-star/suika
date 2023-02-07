1d902036043fa9f6e39111336a13bd32297056b3
xigua <hhbuhuiai@163.com>
2023-02-07 22:25:04 +0800

    feat: remove useless code of rect graph

--- packages/suika/src/editor/scene/rect.ts
+++ packages/suika/src/editor/scene/rect.ts
@@ -1,6 +1,4 @@
-import { IBox, IRect } from '../../type.interface';
-import { getAbsoluteCoords } from '../../utils/graphics';
-import { transformRotate } from '../../utils/transform';
+import { IRect } from '../../type.interface';
 import { Graph, IGraph } from './graph';
 
 export interface RectGraph extends IGraph, IRect {}
@@ -8,40 +6,5 @@ export interface RectGraph extends IGraph, IRect {}
 export class Rect extends Graph {
   constructor(options: RectGraph) {
     super(options);
-  }
-  /**
-   * 计算包围盒（不考虑 strokeWidth）
-   * 考虑旋转
-   */
-  getBBox(): IBox {
-    const [x, y, x2, y2, cx, cy] = getAbsoluteCoords(this);
-    const rotation = this.rotation;
-    if (!rotation) {
-      return this.getBBoxWithoutRotation();
-    }
-
-    const [tlX, tlY] = transformRotate(x, y, rotation, cx, cy); // 左上
-    const [trX, trY] = transformRotate(x2, y, rotation, cx, cy); // 右上
-    const [brX, brY] = transformRotate(x2, y2, rotation, cx, cy); // 右下
-    const [blX, blY] = transformRotate(x, y2, rotation, cx, cy); // 右下
-
-    const minX = Math.min(tlX, trX, brX, blX);
-    const minY = Math.min(tlY, trY, brY, blY);
-    const maxX = Math.max(tlX, trX, brX, blX);
-    const maxY = Math.max(tlY, trY, brY, blY);
-    return {
-      x: minX,
-      y: minY,
-      width: maxX - minX,
-      height: maxY - minY,
-    };
-  }
-  getBBoxWithoutRotation() {
-    return {
-      x: this.x,
-      y: this.y,
-      width: this.width,
-      height: this.height,
-    };
   }
 }
