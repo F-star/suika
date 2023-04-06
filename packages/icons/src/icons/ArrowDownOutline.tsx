19c2e78675cccffaa6664c000f13bb053a1355e8
xigua <hhbuhuiai@163.com>
2023-04-06 08:44:33 +0800

    feat: pnpm

---
+++ packages/icons/src/icons/ArrowDownOutline.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const IconDownOutlined = React.memo(() => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M7 10L12 15L17 10" stroke="#333333" />
+    </svg>
+  );
+});
