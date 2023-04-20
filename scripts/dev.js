f18557ecc185c72d42ebea4ec44deb957c018e70
F-star <hhbuhuiai@163.com>
2023-04-20 13:17:42 +0800

    chore: dev mode

---
+++ scripts/dev.js
@@ -0,0 +1,61 @@
+const { build, context } = require('esbuild');
+const args = require('minimist')(process.argv.slice(2));
+const path = require('path');
+
+const setup = async () => {
+  const target = args._[0];
+  if (!target) {
+    console.log('Please specify a target package');
+    return;
+  }
+
+  const pkgJson = require(path.resolve(
+    __dirname,
+    `../packages/${target}/package.json`,
+  ));
+
+  const outfile = path.resolve(
+    __dirname,
+    `../packages/${target}/dist/${target}.esm.js`,
+  );
+  const relativeOutputFile = path.relative(process.cwd(), outfile);
+
+  /** @type {import('esbuild').BuildContext} */
+  const ctx = await context({
+    entryPoints: [
+      path.resolve(__dirname, `../packages/${target}/src/index.ts`),
+    ],
+    outfile,
+    bundle: true,
+    external: Object.keys({
+      ...pkgJson.dependencies,
+      ...pkgJson.peerDependencies,
+      ...pkgJson.devDependencies,
+    }).filter((dep) => !dep.startsWith('@suika')),
+    sourcemap: true,
+    format: 'esm',
+    platform: 'browser',
+    plugins: [
+      {
+        name: 'watch-build',
+        setup(build) {
+          build.onEnd((result) => {
+            if (result.errors.length) {
+              console.error('build failed:', result.errors);
+            } else {
+              console.log('watch build succeeded:', relativeOutputFile);
+            }
+          });
+        },
+      },
+    ],
+  });
+
+  await ctx.rebuild();
+
+  await ctx.watch(() => {
+    console.log('watch build succeeded:', relativeOutputFile);
+  });
+};
+
+setup();
