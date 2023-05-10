0f9b514db096f02732dfabfdb27173b56970272e
F-star <hhbuhuiai@163.com>
2023-05-10 13:11:52 +0800

    fix: disable undo/redo in contextmenu when reach end of history(#27)

--- packages/suika/src/editor/commands/command_manager.ts
+++ packages/suika/src/editor/commands/command_manager.ts
@@ -1,10 +1,19 @@
+import EventEmitter from '../../utils/event_emitter';
 import { Editor } from '../editor';
 import { ICommand } from './type';
+
+export interface IHistoryStatus {
+  canRedo: boolean;
+  canUndo: boolean;
+}
 
 export class CommandManager {
   redoStack: ICommand[] = [];
   undoStack: ICommand[] = [];
   private isEnableRedoUndo = true;
+  private emitter = new EventEmitter<{
+    change(status: IHistoryStatus): void;
+  }>();
 
   constructor(private editor: Editor) {}
 
@@ -17,12 +26,13 @@ export class CommandManager {
       console.log(
         `%c Redo %c ${command.desc}`,
         'background: #f04; color: #ee0',
-        ''
+        '',
       );
       this.undoStack.push(command);
       command.redo();
 
       this.editor.sceneGraph.render();
+      this.emitStatusChange();
     }
   }
   undo() {
@@ -34,12 +44,13 @@ export class CommandManager {
       console.log(
         `%c Undo %c ${command.desc}`,
         'background: #40f; color: #eee',
-        ''
+        '',
       );
       this.redoStack.push(command);
       command.undo();
 
       this.editor.sceneGraph.render();
+      this.emitStatusChange();
     }
   }
   enableRedoUndo() {
@@ -52,9 +63,22 @@ export class CommandManager {
     console.log(
       `%c Exec %c ${command.desc}`,
       'background: #222; color: #bada55',
-      ''
+      '',
     );
     this.undoStack.push(command);
     this.redoStack = [];
+    this.emitStatusChange();
+  }
+  private emitStatusChange() {
+    this.emitter.emit('change', {
+      canRedo: this.redoStack.length > 0,
+      canUndo: this.undoStack.length > 0,
+    });
+  }
+  on(eventName: 'change', listener: (status: IHistoryStatus) => void) {
+    this.emitter.on(eventName, listener);
+  }
+  off(eventName: 'change', listener: (status: IHistoryStatus) => void) {
+    this.emitter.off(eventName, listener);
   }
 }
