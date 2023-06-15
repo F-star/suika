9f8a0b4dfdfc75c8dc3eac516594d940f5ea081a
xigua <hhbuhuiai@163.com>
2023-06-16 00:38:54 +0800

    feat: move elements by hotkey

--- packages/suika/src/utils/array_util.ts
+++ packages/suika/src/utils/array_util.ts
@@ -5,9 +5,20 @@
  */
 export const forEach = <T>(
   array: T[],
-  cb: (item: T, index?: number) => void
+  cb: (item: T, index?: number) => void,
 ) => {
   for (let i = 0, len = array.length; i < len; i++) {
     cb(array[i], i);
   }
+};
+
+export const ArrMap = <T, U>(
+  array: T[],
+  cb: (item: T, index?: number) => U,
+): U[] => {
+  const ret: U[] = [];
+  for (let i = 0, len = array.length; i < len; i++) {
+    ret.push(cb(array[i], i));
+  }
+  return ret;
 };
