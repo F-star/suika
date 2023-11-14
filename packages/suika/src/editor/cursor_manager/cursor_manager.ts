23c56198edda46619b0b16d251f01b86a04b972d
xigua <hhbuhuiai@163.com>
2023-11-14 23:08:23 +0800

    feat: optimize CursorManager

--- packages/suika/src/editor/cursor_manager/cursor_manager.ts
+++ packages/suika/src/editor/cursor_manager/cursor_manager.ts
@@ -2,7 +2,7 @@ import { normalizeDegree } from '@suika/geo';
 import { Editor } from '../editor';
 import './cursor.scss';
 import isEqual from 'lodash.isequal';
-import { getRotationIconSvgBase64 } from './util';
+import { getRotationIconSvgDataUrl } from './util';
 
 export interface ICursorRotation {
   type: 'rotation';
@@ -26,9 +26,9 @@ export type ICursor =
   | 'zoom-out';
 
 export class CursorManger {
-  private cursor!: ICursor;
+  private cursor: ICursor = 'default';
   // the cursors with custom style, need to add class to canvas element
-  private customCursor = new Set<ICursor>(['default']);
+  private customClassCursor = new Set<ICursor>(['default']);
 
   constructor(private editor: Editor) {
     this.setCursor('default');
@@ -38,45 +38,62 @@ export class CursorManger {
     return this.cursor;
   }
 
+  private normalizeCursor(cursor: ICursor): ICursor {
+    if (typeof cursor === 'string') {
+      return cursor;
+    }
+
+    if (cursor.type === 'resize') {
+      return {
+        type: cursor.type,
+        // degree: 0 ~ 179. e.g 0 is from top to bottom , 90 is from left to right
+        degree: normalizeDegree(cursor.degree) % 180,
+      };
+    }
+
+    if (cursor.type === 'rotation') {
+      return {
+        type: cursor.type,
+        degree: normalizeDegree(Math.round(cursor.degree)),
+      };
+    }
+
+    return cursor;
+  }
+
   setCursor(cursor: ICursor) {
+    cursor = this.normalizeCursor(cursor);
     if (isEqual(cursor, this.cursor)) {
       return;
     }
-    const prevCursor = this.cursor;
+
     this.cursor = cursor;
 
     // custom class cursor
     const clsPrefix = 'suika-cursor-';
-    const className = `${clsPrefix}${cursor}`;
-    const prevClassName = `${clsPrefix}${prevCursor}`;
-    if (className !== prevClassName) {
-      if (this.customCursor.has(prevCursor)) {
-        this.editor.canvasElement.classList.remove(prevClassName);
-      } else {
-        this.editor.canvasElement.style.cursor = '';
-      }
-    }
 
-    if (this.customCursor.has(cursor)) {
-      const prevClassName = `${clsPrefix}${prevCursor}`;
-      if (className !== prevClassName) {
-        this.editor.canvasElement.classList.add(className);
+    const canvasElement = this.editor.canvasElement;
+    canvasElement.classList.forEach((className) => {
+      if (className.startsWith(clsPrefix)) {
+        canvasElement.classList.remove(className);
       }
-    } else {
-      if (typeof cursor == 'string') {
-        this.editor.canvasElement.style.cursor = cursor;
-      } else if (cursor.type === 'resize') {
-        this.setResizeCursorInCanvas(cursor.degree);
-      } else if (cursor.type === 'rotation') {
-        this.setRotationCursorInCanvas(cursor.degree);
-      }
+    });
+    this.editor.canvasElement.style.cursor = '';
+
+    if (this.customClassCursor.has(cursor)) {
+      const className = `${clsPrefix}${cursor}`;
+      this.editor.canvasElement.classList.add(className);
+    } else if (typeof cursor == 'string') {
+      this.editor.canvasElement.style.cursor = cursor;
+    } else if (cursor.type === 'resize') {
+      this.setResizeCursorInCanvas(cursor.degree);
+    } else if (cursor.type === 'rotation') {
+      this.setRotationCursorInCanvas(cursor.degree);
     }
   }
 
   private setResizeCursorInCanvas(degree: number) {
     let styleCursor = '';
-    // degree: 0 ~ 179. 0 is from top to bottom , 90 is from left to right
-    degree = normalizeDegree(degree) % 180;
     if (degree < 22.5) {
       styleCursor = 'ns-resize';
     } else if (degree < 67.5) {
@@ -92,9 +109,7 @@ export class CursorManger {
   }
 
   private setRotationCursorInCanvas(degree: number) {
-    degree = Math.round(degree);
-    // TODO: cache the svg base64
-    this.editor.canvasElement.style.cursor = `url('${getRotationIconSvgBase64(
+    this.editor.canvasElement.style.cursor = `url('${getRotationIconSvgDataUrl(
       degree,
     )}') 16 16, auto`;
   }
