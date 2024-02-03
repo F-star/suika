a5f2751f33b0e600dd28703d9aa700c69d6b601d
xigua <hhbuhuiai@163.com>
2024-02-04 00:18:56 +0800

    chore: package @suika/suika type check

--- packages/suika/vite.config.ts
+++ packages/suika/vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true })],
   base: './',
   server: {
     port: 6167,
