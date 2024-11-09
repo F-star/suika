351c1f9a62e5c979311fe3773f81fe6357022048
xigua <hhbuhuiai@163.com>
2024-11-09 12:08:35 +0800

    fix: text icon should change color when hover

--- packages/icons/src/icons/tool/text-filled.tsx
+++ packages/icons/src/icons/tool/text-filled.tsx
@@ -9,11 +9,11 @@ export const TextFilled = React.memo(() => {
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="11" y="5" width="1" height="14" fill="black" />
-      <rect x="5" y="5" width="13" height="1" fill="black" />
-      <rect x="5" y="5" width="1" height="3" fill="black" />
-      <rect x="17" y="5" width="1" height="3" fill="black" />
-      <rect x="8" y="18" width="7" height="1" fill="black" />
+      <rect x="11" y="5" width="1" height="14" />
+      <rect x="5" y="5" width="13" height="1" />
+      <rect x="5" y="5" width="1" height="3" />
+      <rect x="17" y="5" width="1" height="3" />
+      <rect x="8" y="18" width="7" height="1" />
     </svg>
   );
 });
